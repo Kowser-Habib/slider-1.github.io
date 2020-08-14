c76963c34c731d0391e662ad62fbe1893195f08f
Kowser-Habib <kowserhabib1997@gmail.com>
2020-08-14 19:42:21 +0600

    This is my third project
    Wow!So nice

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    //bxslider part start//
+    $('.kowser').bxSlider({
+        mode: 'fade',
+        speed: '100',
+        autoplay: true,
+    });
+    //bxslider part end//
+});
